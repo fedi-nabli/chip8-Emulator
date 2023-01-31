c5b6d21d4a2d01e5dae07a25858ec2fe7e7b7d7b
Fedi Nabli <fedinabli@gmail.com>
2023-01-31 18:59:31 +0100

    Implemented Chip8 memory

--- src/chip8memory.c
+++ src/chip8memory.c
@@ -1,7 +1,15 @@
 #include "chip8memory.h"
+#include <assert.h>
+
+static void chip8_is_memory_in_bounds(int index)
+{
+  assert(index >= 0 && index < CHIP8_MEMORY_SIZE);
+}
 
 void chip8_memory_set(struct chip8_memory* memory, int index, unsigned char val)
 {
+  chip8_is_memory_in_bounds(index);
+  
   memory->memory[index] = val;
 }
 
